41ee2ed1bb24b1856c0de6fb904e61c4b4fccde3
casotoi <celso.soto@hotmail.com>
2020-08-30 23:37:55 -0400

    Código agregado

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function()x
+  setInversa <- function(inversa) i <<- inversa
+  getInversa <- function() i 
+  list(set = set, get = get, 
+       setInversa = setInversa, 
+       getInversa = getInversa)
 
 }
 
@@ -11,5 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Devuelve la matriz inversa de x
+  j <- x$getInversa()
+  if(!is.null(j)){
+    message("inversa en caché")
+    return(j)
+  }
+  matriz <- x$get()
+  j <- solve(matriz,...)
+  x$setInversa(j)
+  j
 }
